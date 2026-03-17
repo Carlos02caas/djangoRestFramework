85cf97c6ad6772dfb3804d43e44bb103e381ead0
Carlos Alavez <carlos@ticsup.com>
2026-03-16 23:44:24 -0600

    Vistas Genericas para Doctors

---
+++ doctors/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import ListDoctorsView, DetailDoctorView
+
+
+urlpatterns = [
+    path('doctors/', ListDoctorsView.as_view()),
+    path('doctors/<int:pk>/', DetailDoctorView.as_view()),
+]
--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .serializers import DoctorSerializer
+from .models import Doctor
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+# GET /api/doctors/ => List all doctors
+# POST /api/doctors/ => Create a new doctor
+# GET /api/doctors/<int:pk>/ => Get a doctor by id
+# PUT /api/doctors/<int:pk>/ => Update a doctor by id
+# DELETE /api/doctors/<int:pk>/ => Delete a doctor by id
+
+class ListDoctorsView(ListAPIView, CreateAPIView):
+    allowed_methods = ['GET', 'POST']
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.all()
+
+class DetailDoctorView(RetrieveUpdateDestroyAPIView):
+    allowed_methods = ['GET', 'PUT', 'DELETE']
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.all()
+    
